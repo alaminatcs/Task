b80dbf9bb9c83c20c3d3784a6ebe4e45fc0395ee
alaminatcse <alaminedu.cse@gmail.com>
2025-01-07 23:41:48 +0600

    crispy form

--- django-model/config/settings.py
+++ django-model/config/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'meal_list',
+    "crispy_forms",
+    "crispy_tailwind",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
