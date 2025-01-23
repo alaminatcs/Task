39fbfa27554d4e1b7b7a2f540551b9e76862df3c
alaminatcse <alaminedu.cse@gmail.com>
2025-01-23 22:39:29 +0600

    model form

--- django-model/emp_list/models.py
+++ django-model/emp_list/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Employee(models.Model):
     id = models.IntegerField(primary_key= True)
     name = models.CharField(max_length= 20)
-    position = models.TextField()
+    position = models.CharField(max_length= 10)
 
     def __str__(self):
         return f"Name: {self.name} - ID: {self.id} - Position: {self.position}"
--- django-model/emp_list/views.py
+++ django-model/emp_list/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from . import models
+from . import models, forms
 
 # Create your views here.
 def home(request):
@@ -9,3 +9,12 @@ def home(request):
 def remove_emp(request, id):
     models.Employee.objects.get(pk = id).delete()
     return redirect('emp')
+
+def add_emp(request):
+    if request.method == 'POST':
+        form = forms.EmpForm(request.POST)
+        if form.is_valid:
+            form.save()
+    else:
+        form = forms.EmpForm()
+    return render(request, 'add_emp.html', {'data': form})
